383d86a70096163a0acb966273518b54d8711c28
Nick Snyder <nickdsnyder@gmail.com>
2012-01-16 17:00:59 -0800

    main example

---
+++ example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/nicksnyder/go-i18n/i18n"
+)
+
+var (
+	HelloWorld   = i18n.NewMessage("Hello world!", "This message is displayed when the program begins")
+	GoodbyeWorld = i18n.NewMessage("Goodbye world.", "This message is displayed when the program ends")
+)
+
+var locale string
+
+func main() {
+	flag.StringVar(&locale, "locale", "", "The locale to use for translated messages.")
+	flag.Parse()
+	fmt.Println(locale)
+	i18n.SetLocale(locale)
+	fmt.Println(HelloWorld.String())
+	fmt.Println(GoodbyeWorld.String())
+}
