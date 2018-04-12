3023e34292e481666e9c909826a1722425ae1ebd
Nick Snyder <nickdsnyder@gmail.com>
2018-04-12 08:23:02 -0400

    rename file

--- v2/i18n/plural/codegen/generate.sh
+++ v2/i18n/plural/codegen/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-go build && ./codegen -cout ../pluralrules_gen.go -tout ../pluralrules_gen_test.go && \
-    gofmt -w=true ../pluralrules_gen.go && \
-    gofmt -w=true ../pluralrules_gen_test.go && \
+go build && ./codegen -cout ../rules_gen.go -tout ../rules_gen_test.go && \
+    gofmt -w=true ../rules_gen.go && \
+    gofmt -w=true ../rules_gen_test.go && \
     rm codegen
