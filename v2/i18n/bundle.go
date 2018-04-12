ede9524b67b765dc69c5dfcf9238315ea26298f5
Nick Snyder <nickdsnyder@gmail.com>
2018-04-12 16:12:11 -0400

    remove unused

--- v2/i18n/bundle.go
+++ v2/i18n/bundle.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
 
 	"github.com/nicksnyder/go-i18n/v2/i18n/plural"
 	"github.com/nicksnyder/go-i18n/v2/internal"
@@ -95,33 +94,6 @@ func (b *Bundle) ParseMessageFileBytes(buf []byte, path string) (*MessageFile, e
 		return nil, err
 	}
 	return messageFile, nil
-}
-
-func parsePath(path string) (langTag, format string) {
-	formatStartIdx := -1
-	for i := len(path) - 1; i >= 0; i-- {
-		c := path[i]
-		if os.IsPathSeparator(c) {
-			if formatStartIdx != -1 {
-				langTag = path[i+1 : formatStartIdx]
-			}
-			return
-		}
-		if path[i] == '.' {
-			if formatStartIdx != -1 {
-				langTag = path[i+1 : formatStartIdx]
-				return
-			}
-			if formatStartIdx == -1 {
-				format = path[i+1:]
-				formatStartIdx = i
-			}
-		}
-	}
-	if formatStartIdx != -1 {
-		langTag = path[:formatStartIdx]
-	}
-	return
 }
 
 // MustParseMessageFileBytes is similar to ParseMessageFileBytes
