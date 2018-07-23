9a3374df1846e50f1b710416ff73b5f48e224bb6
Nick Snyder <nickdsnyder@gmail.com>
2018-07-22 21:45:10 -0700

    use prefix arg

--- v2/goi18n/common_test.go
+++ v2/goi18n/common_test.go
@@ -3,7 +3,7 @@ package main
 import "io/ioutil"
 
 func mustTempDir(prefix string) string {
-	outdir, err := ioutil.TempDir("", "TestExtractCommand")
+	outdir, err := ioutil.TempDir("", prefix)
 	if err != nil {
 		panic(err)
 	}
