ce00f0bbc42acc0c4995b7afd086c4d71c232ce6
Benoit Masson <benoit.masson@gmail.com>
2019-03-27 15:50:30 +0100

    Test : add tests for nested translations
    + test for translation including a "."
    + test of invalid file (no key, only a value)
    + tests made non-fatal
    + add test name to understand what is happening

--- v2/internal/parse_test.go
+++ v2/internal/parse_test.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 
@@ -9,6 +10,7 @@ import (
 
 func TestParseMessageFileBytes(t *testing.T) {
 	testCases := []struct {
+		name           string
 		file           string
 		path           string
 		unmarshalFuncs map[string]UnmarshalFunc
@@ -16,6 +18,7 @@ func TestParseMessageFileBytes(t *testing.T) {
 		err            error
 	}{
 		{
+			name: "basic test",
 			file: `{"hello": "world"}`,
 			path: "en.json",
 			messageFile: &MessageFile{
@@ -28,23 +31,139 @@ func TestParseMessageFileBytes(t *testing.T) {
 				}},
 			},
 		},
+		{
+			name: "basic test with dot separator in key",
+			file: `{"prepended.hello": "world"}`,
+			path: "en.json",
+			messageFile: &MessageFile{
+				Path:   "en.json",
+				Tag:    language.English,
+				Format: "json",
+				Messages: []*Message{{
+					ID:    "prepended.hello",
+					Other: "world",
+				}},
+			},
+		},
+		{
+			name: "invalid test (no key)",
+			file: `"hello"`,
+			path: "en.json",
+			err:  errors.New("invalid translation file, expected key-values, got a single value"),
+		},
+		{
+			name: "nested test",
+			file: `{"nested": {"hello": "world"}}`,
+			path: "en.json",
+			messageFile: &MessageFile{
+				Path:   "en.json",
+				Tag:    language.English,
+				Format: "json",
+				Messages: []*Message{{
+					ID:    "nested.hello",
+					Other: "world",
+				}},
+			},
+		},
+		{
+			name: "basic test with description",
+			file: `{"notnested": {"description": "world"}}`,
+			path: "en.json",
+			messageFile: &MessageFile{
+				Path:   "en.json",
+				Tag:    language.English,
+				Format: "json",
+				Messages: []*Message{{
+					ID:          "notnested",
+					Description: "world",
+				}},
+			},
+		},
+		{
+			name: "basic test with id",
+			file: `{"key": {"id": "forced.id"}}`,
+			path: "en.json",
+			messageFile: &MessageFile{
+				Path:   "en.json",
+				Tag:    language.English,
+				Format: "json",
+				Messages: []*Message{{
+					ID: "forced.id",
+				}},
+			},
+		},
+		{
+			name: "basic test with description and dummy",
+			file: `{"notnested": {"description": "world", "dummy": "nothing"}}`,
+			path: "en.json",
+			messageFile: &MessageFile{
+				Path:   "en.json",
+				Tag:    language.English,
+				Format: "json",
+				Messages: []*Message{{
+					ID:          "notnested",
+					Description: "world",
+				}},
+			},
+		},
+		{
+			name: "deeply nested test",
+			file: `{"outer": {"nested": {"inner": "value"}}}`,
+			path: "en.json",
+			messageFile: &MessageFile{
+				Path:   "en.json",
+				Tag:    language.English,
+				Format: "json",
+				Messages: []*Message{{
+					ID:    "outer.nested.inner",
+					Other: "value",
+				}},
+			},
+		},
+		{
+			name: "multiple nested test",
+			file: `{"nested": {"hello": "world", "bye": "all"}}`,
+			path: "en.json",
+			messageFile: &MessageFile{
+				Path:   "en.json",
+				Tag:    language.English,
+				Format: "json",
+				Messages: []*Message{{
+					ID:    "nested.hello",
+					Other: "world",
+				}, {
+					ID:    "nested.bye",
+					Other: "all",
+				}},
+			},
+		},
 	}
 	for _, testCase := range testCases {
 		actual, err := ParseMessageFileBytes([]byte(testCase.file), testCase.path, testCase.unmarshalFuncs)
-		if err != testCase.err {
-			t.Fatalf("expected error %#v; got %#v", testCase.err, err)
+		if (err == nil && testCase.err != nil) ||
+			(err != nil && testCase.err == nil) ||
+			(err != nil && testCase.err != nil && err.Error() != testCase.err.Error()) {
+			t.Errorf("%s failed: expected error %#v; got %#v", testCase.name, testCase.err, err)
+			continue
+		}
+		if actual == nil {
+			continue
 		}
 		if actual.Path != testCase.messageFile.Path {
-			t.Fatalf("expected path %q; got %q", testCase.messageFile.Path, actual.Path)
+			t.Errorf("%s failed: expected path %q; got %q", testCase.name, testCase.messageFile.Path, actual.Path)
+			continue
 		}
 		if actual.Tag != testCase.messageFile.Tag {
-			t.Fatalf("expected tag %q; got %q", testCase.messageFile.Tag, actual.Tag)
+			t.Errorf("%s failed: expected tag %q; got %q", testCase.name, testCase.messageFile.Tag, actual.Tag)
+			continue
 		}
 		if actual.Format != testCase.messageFile.Format {
-			t.Fatalf("expected format %q; got %q", testCase.messageFile.Format, actual.Format)
+			t.Errorf("%s failed: expected format %q; got %q", testCase.name, testCase.messageFile.Format, actual.Format)
+			continue
 		}
 		if !reflect.DeepEqual(actual.Messages, testCase.messageFile.Messages) {
-			t.Fatalf("expected %#v; got %#v", testCase.messageFile.Messages, actual.Messages)
+			t.Errorf("%s failed: expected %#v; got %#v", testCase.name, testCase.messageFile.Messages, actual.Messages)
+			continue
 		}
 	}
 }
